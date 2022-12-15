eb5bde3d9bdc7682445bb3f96407e75f40c41412
Filipe <filipegajo95@gmail.com>
2022-12-15 11:26:46 +0000

    Buttons toast messages and theme changes
    -Added placeholder toast messages on buttons login and register
    -Removed top title bar by changing default theme configurations

--- Sermo/app/src/main/java/pt/ipca/sermo/LoginActivity.kt
+++ Sermo/app/src/main/java/pt/ipca/sermo/LoginActivity.kt
@@ -2,10 +2,22 @@ package pt.ipca.sermo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
+    }
+
+    fun doLogin(view: View) {
+        // Placeholder message
+        Toast.makeText(this, "Login Successful!", Toast.LENGTH_LONG).show()
+    }
+
+    fun startRegister(view: View) {
+        // Placeholder message
+        Toast.makeText(this, "Register Successful!", Toast.LENGTH_LONG).show()
     }
 }
