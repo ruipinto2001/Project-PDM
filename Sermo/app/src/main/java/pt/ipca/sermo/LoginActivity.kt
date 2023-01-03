3b6c867d5894843f950e7719e0ab00601f0d8eb4
Filipe <filipegajo95@gmail.com>
2023-01-03 16:19:41 +0000

    Register XML progress

--- Sermo/app/src/main/java/pt/ipca/sermo/LoginActivity.kt
+++ Sermo/app/src/main/java/pt/ipca/sermo/LoginActivity.kt
@@ -1,11 +1,14 @@
 package pt.ipca.sermo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 
 class LoginActivity : AppCompatActivity() {
+    private val code = 1001
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
@@ -16,8 +19,9 @@ class LoginActivity : AppCompatActivity() {
         Toast.makeText(this, "Login Successful!", Toast.LENGTH_LONG).show()
     }
 
-    fun startRegister(view: View) {
-        // Placeholder message
-        Toast.makeText(this, "Register Successful!", Toast.LENGTH_LONG).show()
+    fun startRegister(view: View)
+    {
+        val intent = Intent(this@LoginActivity, RegisterActivity::class.java)
+        startActivityForResult(intent, code)
     }
 }
--- Sermo/app/src/main/java/pt/ipca/sermo/RegisterActivity.kt
+++ Sermo/app/src/main/java/pt/ipca/sermo/RegisterActivity.kt
@@ -2,11 +2,19 @@ package pt.ipca.sermo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+    }
+
+    fun doRegister(view: View)
+    {
+        // Placeholder message
+        Toast.makeText(this, "Register Successful!", Toast.LENGTH_LONG).show()
     }
 
 }
